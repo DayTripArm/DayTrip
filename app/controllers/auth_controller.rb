401e1289e5d97a65c4d19edffd8011d64996d6ed
Albert Karamyan <albert.karamyan@gmail.com>
2020-06-30 02:15:21 +0400

    Add user name to sign in/up API call response

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,6 @@
 class AuthController < ApplicationController
   def login
-      user = Login.find_by(email: params[:email])
+      user = Login.joins(:profile).select("logins.*, profiles.name").find_by(email: params[:email])
       if user && user.authenticate(params[:password])
         render json: payload(user), status: :ok
       elsif user && !user.authenticate(params[:password])
@@ -14,7 +14,7 @@ class AuthController < ApplicationController
 
   def payload(user)
     return nil unless user and user.id
-    user_info = {id: user.id, email: user.email, user_type: user.user_type}
+    user_info = {id: user.id, name: user.profile.name, email: user.email, user_type: user.user_type}
     user_info[:is_prereg] = user.is_prereg if user.user_type == 2
     {
         auth_token: JsonWebToken.encode({id: user.id, email: user.email}, 5.minutes.from_now),
--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -23,7 +23,7 @@ class LoginsController < ApplicationController
 
   def payload(user)
     return nil unless user and user.id
-    user_info = {id: user.id, email: user.email, user_type: user.user_type}
+    user_info = {id: user.id, name: user.profile.name, email: user.email, user_type: user.user_type}
     user_info[:is_prereg] = user.is_prereg if user.user_type == 2
     {
         auth_token: JsonWebToken.encode({id: user.id, email: user.email}, 5.minutes.from_now),
