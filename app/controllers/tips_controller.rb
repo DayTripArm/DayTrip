2b88dc6b1a9368ac97070539d1762c85f29aa4a9
Albert Karamyan <albert.karamyan@gmail.com>
2020-07-09 10:59:18 +0400

    Fixed GET tips API if no tips exists

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,7 +1,7 @@
 class TipsController < ApplicationController
   def index
     tips_category = TipCategory.where(category_type: params[:type]).first
-    tips = TipCategory.find_by(category_type: params[:type]).tips.exclude_tip_fields
+    tips = tips_category.tips.exclude_tip_fields
     render json: {title: tips_category.title, tips: tips}, status: :ok
   end
 end
