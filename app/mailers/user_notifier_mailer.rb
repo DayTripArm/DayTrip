9715753789eb6c6809b16ad36dc98a59f0634ed8
Albert Karamyan <albert.karamyan@gmail.com>
2020-08-18 15:39:00 +0400

    Fixed issue on sending notify email to admins while completing driver preregistration

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,7 +1,7 @@
 class UserNotifierMailer < ApplicationMailer
   def notify_admins(user, car_details)
     @user = user
-    @car_details  = car_details
+    @car_details  = car_details || []
     AdminUser.all.each do |admin|
         mail(from: @user.email, to: admin.email,  subject: "The driver #{@user.name} has changed car details")
     end
