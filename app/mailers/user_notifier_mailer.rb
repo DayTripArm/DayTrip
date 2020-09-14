e303fa9c65448be797b93b44e86268a7f6ef064a
Maryam Kharamyan <mkharamy@gmail.com>
2020-09-14 17:48:28 +0400

    DAYTRIP - 214 and 215: Added email templates to send to system admins whenever a new driver signed up or changed car information.

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -4,14 +4,14 @@ class UserNotifierMailer < ApplicationMailer
   def notify_admins_car_details(login_id)
     @user = Profile.user_basic_info(login_id)
     AdminUser.all.each do |admin|
-        mail(from: @user.email, to: admin.email,  subject: "DayTrip Armenia: Account Temporarily Suspended")
+        mail(from: @user.email, to: admin.email,  subject: "DayTrip Armenia: Driver’s Account Changes")
     end
   end
 
   def notify_admins_prereg(login_id)
     @user = Profile.user_basic_info(login_id)
     AdminUser.all.each do |admin|
-      mail(from: @user.email, to: admin.email,  subject: "Registration completed")
+      mail(from: @user.email, to: admin.email,  subject: "DayTrip Armenia: New Driver")
     end
   end
 
@@ -49,4 +49,5 @@ class UserNotifierMailer < ApplicationMailer
     @user = Profile.user_basic_info(login_id)
     mail(to: @user.email, subject: 'DayTrip Armenia: Account Changes Rejected')
   end
+
 end
