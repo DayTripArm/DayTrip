1e5e57a30734ca4cd33dfb1aa8871101b785a4a9
Albert Karamyan <albert.karamyan@gmail.com>
2020-09-23 03:57:54 +0400

    DT-126 Back-end for search for drivers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,10 +12,11 @@ class HomeController < ApplicationController
   def search_drivers
     errors = []
     begin
-      drivers_list = {}
+      drivers_list = []
       drivers = Login.joins(:profile, :driver_info, :photos)
-                    .select("logins.id, profiles.name AS driver_name, driver_infos.car_specs")
-                    .where("logins.user_type = 2").distinct
+                    .select("logins.id, profiles.name AS driver_name, profiles.languages, driver_infos.car_seats, driver_infos.car_specs,
+                            driver_infos.hit_the_road_tariff, driver_infos.car_mark, driver_infos.car_model")
+                    .where("logins.user_type = 2 AND driver_infos.hit_the_road_tariff is not null").distinct
 
       unless params[:travelers].blank?
         drivers = drivers.where("car_seats >= ?", params[:travelers].to_i)
@@ -25,18 +26,22 @@ class HomeController < ApplicationController
       end
       drivers.each_with_index do |driver, index|
         drivers_list[index] = driver.to_json
+        drivers_list[index] = JSON.parse(drivers_list[index])
         drivers_photos = driver.photos.where(file_type: [1,2])
+        drivers_list[index].merge!({:profile_photos => {}, :car_photos => []})
         drivers_photos.each do |photo|
           photo.full_path = PhotosHelper::get_photo_full_path(photo.name,  Photo::FILE_TYPES.key(photo.file_type), photo[:login_id].to_s)
+          drivers_list[index][:profile_photos] = photo if photo.file_type == 1
+          drivers_list[index][:car_photos] << photo if photo.file_type == 2
         end
-        drivers_list[index] = JSON.parse(drivers_list[index])
-        drivers_list[index][:drivers_photos] = drivers_photos
+        drivers_list[index]["car_specs"] = JSON.parse(drivers_list[index]["car_specs"])
+        drivers_list[index][:car_full_name] = CarHelper::format_car_model(driver[:car_mark].to_i, driver[:car_model].to_i)
       end
       unless params[:trip_id].blank?
         trip_details = Trip.select('images, title, trip_duration, start_location').where(id: params[:trip_id])
       end
 
-      render json: {trip_details: trip_details, drivers: drivers_list}, status: :ok
+      render json: {trip_details: trip_details, drivers_list: drivers_list}, status: :ok
     rescue StandardError, ActiveRecordError => e
       errors << e.message unless e.message.blank?
       render json: errors, status: :internal_server_error
