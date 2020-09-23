d46a816ac5951381d9b55eb6e7968cebb1744d9c
Albert Karamyan <albert.karamyan@gmail.com>
2020-09-23 15:13:34 +0400

    DT-216 Change tip type for Hit the road help slider

--- app/models/tip_category.rb
+++ app/models/tip_category.rb
@@ -9,7 +9,8 @@ class TipCategory < ApplicationRecord
 # Tip categories. Each Tip type should be uniq.
   CAR_UPLOAD = 1
   DRIVER_PRICE = 2
-  HIT_THE_ROAD = 3
+  HIT_THE_ROAD_DRIVER = 3
+  HIT_THE_ROAD_HOME = 4
 
-  TIPS_TYPE = [['Car upload tip', CAR_UPLOAD], ['Driver price tip', DRIVER_PRICE], ['Hit the Road tip', HIT_THE_ROAD]]
+  TIPS_TYPE = [['Car upload tip', CAR_UPLOAD], ['Driver price tip', DRIVER_PRICE], ['Hit the Road tip for Drivers', HIT_THE_ROAD_DRIVER], ['Hit the Road tip for Home Page', HIT_THE_ROAD_HOME]]
 end
