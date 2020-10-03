e1acb6a008538f57b8b4b8b5c177bfee8a9b7f21
Albert Karamyan <albert.karamyan@gmail.com>
2020-10-03 19:11:57 +0400

    Adjusted search drivers API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,20 +17,24 @@ class HomeController < ApplicationController
                     .select("logins.id, logins.user_type, profiles.name AS driver_name, profiles.languages, driver_infos.car_seats, driver_infos.car_specs,
                             driver_infos.hit_the_road_tariff, driver_infos.tariff1, driver_infos.tariff1, driver_infos.tariff2,
                             driver_infos.car_mark, driver_infos.car_model")
-                    .where("logins.user_type = 2 AND driver_infos.hit_the_road_tariff is not null").distinct
+                    .where("logins.user_type = 2").distinct
 
       unless params[:travelers].blank?
         drivers = drivers.where("car_seats >= ?", params[:travelers].to_i)
       end
       unless params[:date].blank?
-        drivers = drivers.joins(:calendar_setting).where.not("calendar_settings.unavailable_days ->> 'excluded_days' LIKE ?", "%#{params[:date]}%")
+
+        drivers = drivers.joins(:calendar_setting).where
+                      .not("calendar_settings.unavailable_days ->> 'excluded_days' LIKE ?", "%#{params[:date]}%")
       end
+      drivers = drivers.limit(params[:limit] || 10).offset(params[:offset] || 0)
       unless params[:trip_id].blank?
         trip_details = Trip.select('images, title, trip_duration, start_location').where(id: params[:trip_id])
         drivers = drivers.order("tariff1 #{params[:sort] || 'ASC'}, tariff2 #{params[:sort] || 'ASC'}")
+      else
+        drivers = drivers.where("AND driver_infos.hit_the_road_tariff is not null")
+                      .order(hit_the_road_tariff: params[:sort] || 'ASC').distinct
       end
-      drivers = drivers.limit(params[:limit] || 10).offset(params[:offset] || 0)
-      drivers = drivers.order(hit_the_road_tariff: params[:sort] || 'ASC')
 
       drivers.each_with_index do |driver, index|
         drivers_list[index] = driver.to_json
