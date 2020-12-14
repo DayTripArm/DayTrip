74f060201cf896a72d5ee793b0e3fb04569caa02
Maryam Kharamyan <mkharamy@gmail.com>
2020-12-14 16:23:10 +0400

    DAYTRIP-322: Added 'hit_the_road_tariff' field in model to fetch the data.

--- app/models/driver_info.rb
+++ app/models/driver_info.rb
@@ -2,5 +2,5 @@ class DriverInfo < ApplicationRecord
   belongs_to :logins, optional: true
   scope :car_details, -> {select("id, car_type, car_mark, car_model, car_year, car_color")}
   scope :more_details, -> {select("id, car_seats, car_specs, driver_destinations")}
-  scope :prices, -> {select("id, tariff1,tariff2")}
+  scope :prices, -> {select("id, tariff1,tariff2, hit_the_road_tariff")}
 end
