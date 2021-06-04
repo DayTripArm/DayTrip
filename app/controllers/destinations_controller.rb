08324dc452e7b2f96684e54379b88c0b5f44409f
Albert Karamyan <albert.karamyan@gmail.com>
2021-06-05 01:15:36 +0400

    Filter destination by language

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,6 +1,7 @@
 class DestinationsController < ApplicationController
   def index
     destinations = Destination.published.all
+    destinations = destinations.where(lang: params[:lang]).all unless params[:lang].blank?
     render json: {destinations: destinations}, status: :ok
   end
 end
